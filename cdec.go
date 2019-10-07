4edc01cb83fe80a68683b2fe4dd0373f6bfb8edf
camsn0w <camhconnell@gmai.com>
2019-10-06 21:05:12 -0600

    API step 1

--- cdec.go
+++ cdec.go
@@ -1,12 +1,49 @@
 package cdec
 
-import "io"
+import (
+	"bufio"
+	"errors"
+	"io"
+	"strings"
+)
 
-func ScanFreqsFromReader(io.Reader)(FreqCount,error){
+func ScanFreqsFromReader(reader io.Reader) (FreqCount, error) {
+	wordCount := make(map[string]float64)
+	numWords := 0
+	var err error
+	scannedReader := bufio.NewScanner(reader)
+	if reader == nil {
+		err = errors.New("file is empty")
+		freqErr := FreqCount{wordCount, 0}
+		return freqErr, err
+	}
 
+	for scannedReader.Scan() {
+
+		words := strings.Fields(scannedReader.Text())
+
+		//fmt.Println(words)
+		c := 0
+
+		for index, vals := range words {
+
+			count := wordCount[vals]
+			wordCount[words[index]] = count + 1
+			numWords = numWords + 1
+			c++
+		}
+
+	}
+	freqToRet := FreqCount{wordCount, numWords}
+	return freqToRet, err
+}
+func FreqOfWord(word string, freqMap FreqCount) float64 {
+	//	 var freq float64 = float64(wordCount[word]) / float64(numWords) * 100
+	freq := (freqMap.Freqs[word] / float64(freqMap.numWords)) * 100
+	return freq
 }
 
 type FreqCount struct {
-	Freqs map[string]float64
-	secretvar int
+	Freqs    map[string]float64
+	numWords int
 }
--- cmd/filefreqcli.go
+++ cmd/filefreqcli.go
@@ -2,20 +2,75 @@ package main
 
 import (
 	"bufio"
-	"os"
+	"cdec"
+	"flag"
 	"fmt"
+	"os"
 	"strings"
 )
 
-func userInput() (string,error) {
+func fileRead(fileName string) *os.File {
+
+	print("Filename: " + fileName + "\n")
+	inFile, err := os.Open(fileName)
+
+	//T , err := os.Open("testFile.txt")
+	if err != nil {
+		print("ERROR during fileRead ")
+		print(err.Error())
+
+		return nil
+	}
+	//defer inFile.Close()
+	return inFile
+}
+func userInput() (string, error) {
+	fmt.Printf("%s", "Enter a word to search for: ")
 	inpt := bufio.NewReader(os.Stdin)
 	userString, userStringError := inpt.ReadString('\n')
 	if userStringError != nil {
-		fmt.Println(userStringError)
+		errorWithInput(userStringError)
 	}
-	userString = strings.Replace(userString, "\n","",-1)
-	return userString,userStringError
+	userString = strings.Replace(userString, "\n", "", -1)
+	return userString, userStringError
+}
+func errorWithInput(userStringError error) string {
+	for {
+		fmt.Printf("Error: %s; %s\n", userStringError, "; Type 'y' to try again or 'q' to quit")
+		inpt, err := userInput()
+		if err != nil {
+			print("Input must be either 'y' or 'q'\n")
+			continue
+		} else {
+			if inpt == "y" {
+				fmt.Printf("%s", "Enter a word to search for: ")
+				word, err := userInput()
+				if err != nil {
+					fmt.Printf("Error: %s", err)
+					continue
+				} else {
+					return word
+				}
+			}
+		}
+
+	}
 }
 func main() {
-	
+	word, _ := userInput()
+	flag.Parse()
+	fileName := flag.Arg(0)
+	inputFile := fileRead(fileName)
+	var reader = bufio.NewReader(inputFile)
+	freqData, err := cdec.ScanFreqsFromReader(reader)
+	if err != nil {
+
+		fmt.Printf("%s", err)
+		inputFile.Close()
+		os.Exit(1)
+
+	}
+	relativeFreq := cdec.FreqOfWord(word, freqData)
+	fmt.Printf("Frequency of %s is %f", word, relativeFreq)
+
 }
